dc0a53356b05f40c0641556919c046e74a75fd87
Emnet Alemayehu <emnetalem21@gmail.com>
2023-10-16 00:39:52 -0700

    Modifications to Profile page, Main Page

---
+++ SwiftUI-ParkinSense/SwiftUI-ParkinSense/Core/MainPage/MenuBar.swift
@@ -0,0 +1,18 @@
+//
+//  MenuBar.swift
+//  SwiftUI-ParkinSense
+//
+//  Created by Emnet Alemayehu on 10/16/23.
+//
+
+import SwiftUI
+
+struct MenuBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuBar()
+}
