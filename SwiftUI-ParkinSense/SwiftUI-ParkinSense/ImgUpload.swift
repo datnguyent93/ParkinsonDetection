3fd786a947c97dd50038573c5a800296d5f8f34b
Francesca Cueto <76602827+francue@users.noreply.github.com>
2023-10-16 16:00:13 -0700

    fixed random number generator
    wasn't generating a new number with each image upload before

--- SwiftUI-ParkinSense/SwiftUI-ParkinSense/ImgUpload.swift
+++ SwiftUI-ParkinSense/SwiftUI-ParkinSense/ImgUpload.swift
@@ -14,19 +14,20 @@ struct ImgUpload: View {
     @State private var myImage: Image? = nil
     @State private var myImageData: Data? = nil
     
-    let randomInt = Int.random(in: 1..<4)
+    @State var randomInt = 0
     
     var body: some View {
         VStack{
-            if let myImage{
-                Text("\(randomInt)") //prints random int when chosen image is loaded
-                    myImage
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 500, height: 500)
+            if let myImage{ //display selected img and random number
+                Text("\(randomInt)")
+                
+                myImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 500, height: 500)
             }
             
-            PhotosPicker(
+            PhotosPicker( //lets user pick a photo from photo library
                 "Select Image",
                 selection: $mySelection,
                 matching: .images,
@@ -34,10 +35,12 @@ struct ImgUpload: View {
             )
             
         }
-        .onChange(of: mySelection, initial: false){ oldItem, newItem in
-            Task{
-                if let data = try? await newItem?.loadTransferable(type: Data.self){
+        .onChange(of: mySelection, initial: false){ oldItem, newItem in //tracks if new img was selected
+            Task{ //replaces chosen image with new selection
+                if let data = try? await
+                    newItem?.loadTransferable(type: Data.self){
                     if let uiImage = UIImage(data: data){
+                        randomInt = Int.random(in: 1..<4)
                         myImage = Image(uiImage: uiImage)
                         return
                     }
