90ab082e18b2f64cd73e732bfdb649f0d618fedb
Emnet Alemayehu <emnetalem21@gmail.com>
2023-10-17 01:47:45 -0700

    Added opensource spiral doted imager, tracing tests, and result

---
+++ SwiftUI-ParkinSense/SwiftUI-ParkinSense/Image + Tracing/OpenSource SpiralDoted IMG/SpiralView.swift
@@ -0,0 +1,100 @@
+//
+//  SpiralView.swift
+//  SwiftUI-ParkinSense
+//
+//  OPENSOURCE
+//
+
+
+import SwiftUI
+
+
+/// View with placement of contents on spiral points.
+struct SpiralView<Content: View>: View {
+    
+    private let startAt: Angle
+    private let endAt: Angle
+    private let smoothness: CGFloat
+    private let offsetRadius: CGFloat
+    private let offsetAngle: Angle
+    @Binding private var pointsCount: Int
+    @ViewBuilder private let content: (Int, SpiralPoint) -> Content
+    
+    /// Creates a view with placement of contents on spiral points.
+    /// - Parameters:
+    ///   - startAt: the start angle of a spiral.
+    ///   - endAt: the end angle of a spiral.
+    ///   - smoothness: the point density which describes a spiral.
+    ///   - offsetRadius: spiral radius offset for each point with respect to the calculated angle and frame.
+    ///   - offsetAngle: angle offset of each spiral point with respect to the calculated angle and frame.
+    ///   - pointsCount: the number of spiral points.
+    ///   - content: the content view.
+    public init(
+        startAt: Angle = .degrees(90),
+        endAt: Angle = .degrees(450),
+        smoothness: CGFloat = 12,
+        offsetRadius: CGFloat = 0,
+        offsetAngle: Angle = .zero,
+        pointsCount: Binding<Int> = .constant(0),
+        @ViewBuilder content: @escaping (Int, SpiralPoint) -> Content
+    ) {
+        self.startAt = startAt
+        self.endAt = endAt
+        self.smoothness = smoothness
+        self.offsetRadius = offsetRadius
+        self.offsetAngle = offsetAngle
+        _pointsCount = pointsCount
+        self.content = content
+    }
+    
+    public var body: some View {
+        GeometryReader { proxy in
+            if let spiralPoints = spiralPoints(in: proxy.size) {
+                ForEach(0..<spiralPoints.count, id: \.self) { index in
+                    content(index, spiralPoints[index])
+                }
+            }
+        }
+    }
+    
+    private func spiralPoints(in size: CGSize) -> [SpiralPoint]? {
+        let spiralPoints = [SpiralPoint](
+            in: .init(origin: .zero, size: size),
+            startAt: startAt,
+            endAt: endAt,
+            smoothness: smoothness,
+            offsetRadius: offsetRadius,
+            offsetAngle: offsetAngle
+        )
+        
+        pointsCount = spiralPoints?.count ?? 0
+        
+        return spiralPoints
+    }
+}
+
+struct SpiralImage: View {
+    var body: some View {
+        let text = "."
+        
+            SpiralView(
+                startAt: .degrees(180),
+                endAt: .degrees(1305)
+            ) { index, spiralPoint in
+                let startIndex = text.index(text.startIndex, offsetBy: (index % text.count))
+                Text(text[startIndex...startIndex])
+                    .position(x: spiralPoint.point.x, y: spiralPoint.point.y)
+                    .font(.system(size: 100))
+                    
+            }
+            .font(.title.bold())
+            .frame(width: 900, height: 700)
+            .padding()
+        
+    }
+}
+
+#Preview {
+    SpiralImage()
+}
+
