ab7dbcecefa0ad4b393d1b67145961308e0d3ecd
Alex Akoopie <alexakoopie@gmail.com>
2023-10-19 23:17:30 -0700

    Added PythonKit framework

--- SwiftUI-ParkinSense/SwiftUI-ParkinSense/App/ContentView.swift
+++ SwiftUI-ParkinSense/SwiftUI-ParkinSense/App/ContentView.swift
@@ -5,7 +5,22 @@
 //  Created by user242759 on 04/07/1402 AP.
 //
 import SwiftUI
+import Python
 
+guard let stdLibPath = Bundle.main.path(forResource: "python-stdlib", ofType: nil) else { return }
+guard let libDynloadPath = Bundle.main.path(forResource: "python-stdlib/lib-dynload", ofType: nil) else { return }
+setenv("PYTHONHOME", stdLibPath, 1)
+setenv("PYTHONPATH", "\(stdLibPath):\(libDynloadPath)", 1)
+Py_Initialize()
+// we now have a Python interpreter ready to be used
+import Python
+
+guard let stdLibPath = Bundle.main.path(forResource: "python-stdlib", ofType: nil) else { return }
+guard let libDynloadPath = Bundle.main.path(forResource: "python-stdlib/lib-dynload", ofType: nil) else { return }
+setenv("PYTHONHOME", stdLibPath, 1)
+setenv("PYTHONPATH", "\(stdLibPath):\(libDynloadPath)", 1)
+Py_Initialize()
+// we now have a Python interpreter ready to be used
 struct ContentView: View {
     var body: some View {
         NavigationView{
